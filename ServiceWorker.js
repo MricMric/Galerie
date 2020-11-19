5b154d7d8fce21678ad72feac58145dff6c3ec64
MricMric <aymeric.delabarre@gmail.com>
2020-11-19 09:12:57 +0100

    Modif ServiceWorker

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -6,8 +6,27 @@ self.addEventListener('message', event => {
 });
 
 
-self.addEventListener('fetch', event => {
-    event.RespondWith(new Response('PWA!!!!'));
+self.addEventListener("fetch", (event) => {
+    const url = event.request.url;
+    
+    if (url.indexOf("https://quirky-kirch-74f858.netlify.app/images.json") ===0) {
+        event.respondWith(fetch(event.request).then((response) => {
+
+            if (response.statusText !== "OK") {console.error(
+                "Service Worker","Error when fetching",event.request.url);
+                return response;
+            }
+            
+            console.info("Formatting data");
+
+                return response.json().then((json) => {
+                    const formattedResponse = json.map((j) => ({
+                    name: j.name,description: j.description || "",updated_at: j.updated_at,}));
+                    return new Response(JSON.stringify(formattedResponse));
+                });
+
+        }));
+    }            
 });
 
 
