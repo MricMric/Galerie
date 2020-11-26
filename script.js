77a2206ff5cdbe061c1c48aa75b83766bf39142e
MricMric <aymeric.delabarre@gmail.com>
2020-11-26 16:15:12 +0100

    fix

--- script.js
+++ script.js
@@ -22,9 +22,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 let div_image = document.createElement("div");
                 div_image.classList.add("col-sm-6", "col-md-4", "col-lg-3", "p-2");
 
-                /*let button_image = document.createElement("button");
-                button_image.classList.add("btn", "btn-warning");*/
-
                 let img = document.createElement("img");
                 img.src = image.src;
                 img.alt = image.alt;
@@ -34,9 +31,13 @@ document.addEventListener("DOMContentLoaded", function () {
                 let div_fav = document.createElement("div");
                 div_fav.classList.add("p-1");
 
-                let span_fav = document.createElement("div");
+                let span_fav = document.createElement("span");
                 span_fav.classList.add("fav-icon");
 
+                let icon_fav = document.createElement("i");
+                icon_fav.classList.add("far", "fa-hearth");
+
+                span_fav.appendChild(icon_fav);
                 div_fav.appendChild(span_fav);
                 div_image.appendChild(img);
                 div_image.appendChild(div_fav);
